9d2131fda0d035c2010d6e1cc1576ba1e0e08ddf
Seyedmajid Etehadi <ettehadi.m@gmail.com>
2019-12-07 17:54:23 +0330

    Add build buster script

---
+++ build-buster.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Read arg from build.properties
+
+DOCKER_VERSION=buster
+
+set -x
+. build.properties
+docker build --force-rm  \
+    -t $REPO_NAME:$DOCKER_VERSION \
+    --build-arg TIMEZONE=$TIMEZONE \
+    --build-arg DOCKER_VERSION=$DOCKER_VERSION .
